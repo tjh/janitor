be524143899c7aab4debd39353ae98f3e3e7efd2
Tim Harvey <tharvey@squaremouth.com>
2010-09-04 07:53:52 -0500

    Bugfix: correct parameter order in tests

--- spec/lib/janitor_spec.rb
+++ spec/lib/janitor_spec.rb
@@ -8,7 +8,7 @@ describe "Janitor" do
       finder = mock('finder')
       finder.should_receive(:number_of_matches).with(pattern).and_return(0)
       Finder.should_receive(:new).with(path).and_return(finder)
-      Janitor.count(path, pattern)
+      Janitor.count(pattern, path)
     end
     
     it "should raise an exception when number_of_matches returns a non-zero count" do
@@ -17,7 +17,7 @@ describe "Janitor" do
       finder = mock('finder')
       finder.should_receive(:number_of_matches).with(pattern).and_return(1)
       Finder.should_receive(:new).with(path).and_return(finder)
-      lambda { Janitor.count(path, pattern) }.should raise_error
+      lambda { Janitor.count(pattern, path) }.should raise_error
     end
   end
 end
